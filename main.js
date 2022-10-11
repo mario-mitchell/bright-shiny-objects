3dd07aa5c8e0056c4e0f52a084cfc2b83766e401
mario-mitchell <35703472+mario-mitchell@users.noreply.github.com>
2022-10-11 09:30:30 -0700

    Turning in assignment.

--- main.js
+++ main.js
@@ -3,7 +3,82 @@
 // └─────────────────┘
 
 
+// CREATE USER
+function createUser(firstName, lastName){
+    let user = {
+        firstName: firstName,
+        lastName: lastName
+    }
+    return user;
+}
+console.log(createUser("Mario", "Mitchell"));
+// SET AGE
+function setAge(user, age){
+    user.age = age;
+    return user
+}
+console.log(setAge(createUser("Mario", "Mitchell"), 31));
+let mrUser = {
+    firstName: "Nathalie",
+    lastName: "Mitchell"
+}
+console.log(setAge(mrUser, 31))
+// INCREMENT AGE
+function incrementAge(user){
+    user.age = user.age + 1;
+    return user;
+}
+console.log(incrementAge(mrUser));
+// FIX CAR
+function fixCar(car){
+    car.needsMaintenance = false
+    return car;
+}
+const car = {
+    make: "Tesla",
+    model: "Model S",
+    year: 2022,
+    needsMaintenance: true
+}
+console.log(fixCar(car));
+// ADD GRADES
+    // concat()
+function addGrades(student, updates){
+    student.grades = student.grades.concat(updates);
+    return student;
+}
+    // push()
+function adddddGrades(student, updates){
+    for (let i = 0; i < updates.length; i++){
+        student.grades.push(updates[i]);
+    }
+    return student
+}
+const student = {
+    name: 'Nathalie Mitchell',
+    email: 'nathalie@gmail.com',
+    grades: [80, 100, 95]
+};
+const student2 = {
+    name: 'Mario Mitchell',
+    email: 'mario.mitchell@codeimmersives.com',
+    grades: [80, 100, 95]
+};
+const newGrades = [88, 70, 90];
+console.log(addGrades(student, newGrades));
+console.log(adddddGrades(student2, newGrades));
+// GET DATA TYPE
+function getDataType(obj, key){
+    return typeof obj[key]
+}
+console.log(getDataType(car, "make"));
 
+// ADD TODO
+function addTodo(todo, newItem){
+    todos.push(newItem);
+    todo.push(newItem);
+    return todo
+}
 
 
 
